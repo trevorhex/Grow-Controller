1c6e6ad67c311ec0e06fe20e096ce267e381a6d3
Trevor Smith <trevorsmith@onetable.org>
2025-07-12 16:15:38 -0700

    connect data api

--- api.py
+++ api.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return '<h1>Grow Controller</h1>'
+def get_db_connection():
+  conn = sqlite3.connect('data.db')
+  conn.row_factory = sqlite3.Row
+  return conn
+
+@app.route('/data', methods=['GET'])
+def data():
+  conn = get_db_connection()
+  try:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM data")
+    rows = cursor.fetchall()
+    data = []
+    for row in rows:
+      data.append({
+        'co2': row[0],
+        'temperature': row[1],
+        'humidity': row[2],
+        'timestamp': row[3]
+      })
+    return jsonify(data)
+  finally:
+    conn.close()
--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,19 +1,29 @@
 import { ReactNode } from 'react'
 import type { Metadata } from 'next'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+import { API_URL } from '../api/config'
+
 export const metadata: Metadata = {
-  title: 'Grow Controller'
+  title: 'Grow Controller Dashboard'
 }
 
-export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+export default async function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+  const response = await fetch(`${API_URL}/data`)
+  const data = await response.json()
+
+  console.log('Data fetched:', data)
+
   return <html lang="en">
     <body>
-      {children}
+      <AppRouterCacheProvider>
+        {children}
+      </AppRouterCacheProvider>
     </body>
   </html>
 }
